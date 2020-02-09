322adfdaf497225fd107549a2fa6906c667a8703
Arne Jansen <arne@die-jansens.de>
2020-02-09 16:01:28 +0000

    wip

--- src/fpga.c
+++ src/fpga.c
@@ -4,157 +4,157 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
+#include "autoconf.h" // CONFIG_CLOCK_FREQ
 #include "board/gpio.h" // gpio_out_write
 #include "basecmd.h" // oid_alloc
 #include "command.h" // DECL_COMMAND
 #include "sched.h" // DECL_SHUTDOWN
 #include "spicmds.h" // spidev_transfer
+#include "board/irq.h" // irq_poll
+#include "board/misc.h" // timer_from_us
+
+static struct task_wake fpga_wake;
 
 struct fpga_s {
+    struct gpio_out clk;
+    struct gpio_in miso;
+    struct gpio_out mosi;
+    struct gpio_out cs;
     struct gpio_out program;
-    struct gpio_out init;
-    struct gpio_out done;
-    struct gpio_out hold;
-    uint8_t spi_bus;
-    uint8_t flash;
+    struct gpio_in init;
+    struct gpio_in done;
+    struct gpio_out di;
+    uint8_t         state;
+    uint32_t        cnt;
 };
+
+#define FS_INIT_FLASH       0
+#define FS_INIT_FPGA        1
+#define FS_TRANSFER         2
+#define FS_SEND_RESPONSE    3
+#define FS_INITIALIZED      4
+
+static uint32_t
+nsecs_to_ticks(uint32_t ns)
+{
+    return timer_from_us(ns * 1000) / 1000000;
+}
+
+static inline void
+ndelay(uint32_t nsecs)
+{
+    if (CONFIG_CLOCK_FREQ <= 48000000)
+        // Slower MCUs don't require a delay
+        return;
+    uint32_t end = timer_read_time() + nsecs_to_ticks(nsecs);
+    while (timer_is_before(timer_read_time(), end))
+        irq_poll();
+}
 
 void
 command_config_fpga(uint32_t *args)
 {
-    struct fpga_s *fpga = oid_alloc(args[0], command_config_fpga,
-                                    sizeof(*fpga));
-    fpga->flash = args[1];
-    fpga->program = gpio_out_setup(args[2], 1);
-    fpga->init = gpio_out_setup(args[3], 1);
-    fpga->done = gpio_out_setup(args[4], 1);
-    fpga->hold = gpio_out_setup(args[5], 1);
-    fpga->spi_bus = args[6];
+    struct fpga_s *f = oid_alloc(args[0], command_config_fpga,
+                                    sizeof(*f));
+    /* spi to flash */
+    f->clk = gpio_out_setup(args[1], 0);
+    f->miso = gpio_in_setup(args[2], 0);
+    f->mosi = gpio_out_setup(args[3], 0);
+    f->cs = gpio_out_setup(args[4], 1);
+    /* slave serial to fpga */
+    f->program = gpio_out_setup(args[5], 0);
+    f->init = gpio_in_setup(args[6], 1);
+    f->done = gpio_in_setup(args[7], 1);
+    f->di = gpio_out_setup(args[8], 1);
 
 	output("fpga setup called with oid %u", args[0]);
+
+    // wait a 100us before starting
+    ndelay(100000);
+    f->state = FS_INIT_FLASH;
+
+    sched_wake_task(&fpga_wake);
 }
 DECL_COMMAND(command_config_fpga,
-             "config_fpga oid=%c flash_spi=%u program_pin=%u init_pin=%u "
-             " done_pin=%u hold_pin=%u spi_bus=%u");
-
-#if 0
-struct spidev_s *
-spidev_oid_lookup(uint8_t oid)
-{
-    return oid_lookup(oid, command_config_spi);
-}
+             "config_fpga oid=%c clk_pin=%u miso_pin=%u mosi_pin=%u cs_pin=%u "
+             "program_pin=%u init_pin=%u done_pin=%u di_pin=%u");
 
 void
-command_spi_set_bus(uint32_t *args)
+fpga_task(void)
 {
-    struct spidev_s *spi = spidev_oid_lookup(args[0]);
-    uint8_t mode = args[2];
-    if (mode > 3 || spi->flags & (SF_SOFTWARE|SF_HARDWARE))
-        shutdown("Invalid spi config");
-    spi->spi_config = spi_setup(args[1], mode, args[3]);
-    spi->flags |= SF_HARDWARE;
-}
-DECL_COMMAND(command_spi_set_bus,
-             "spi_set_bus oid=%c spi_bus=%u mode=%u rate=%u");
-
-void
-spidev_set_software_bus(struct spidev_s *spi, struct spi_software *ss)
-{
-    if (spi->flags & (SF_SOFTWARE|SF_HARDWARE))
-        shutdown("Invalid spi config");
-    spi->spi_software = ss;
-    spi->flags |= SF_SOFTWARE;
-}
-
-void
-spidev_transfer(struct spidev_s *spi, uint8_t receive_data
-                , uint8_t data_len, uint8_t *data)
-{
-    if (!(spi->flags & (SF_SOFTWARE|SF_HARDWARE)))
-        // Not yet initialized
+    if (!sched_check_wake(&fpga_wake))
         return;
 
-    if (CONFIG_HAVE_GPIO_BITBANGING && spi->flags & SF_SOFTWARE)
-        spi_software_prepare(spi->spi_software);
-    else
-        spi_prepare(spi->spi_config);
-
-    if (spi->flags & SF_HAVE_PIN)
-        gpio_out_write(spi->pin, 0);
-
-    if (CONFIG_HAVE_GPIO_BITBANGING && spi->flags & SF_SOFTWARE)
-        spi_software_transfer(spi->spi_software, receive_data, data_len, data);
-    else
-        spi_transfer(spi->spi_config, receive_data, data_len, data);
-
-    if (spi->flags & SF_HAVE_PIN)
-        gpio_out_write(spi->pin, 1);
-}
-
-void
-command_spi_transfer(uint32_t *args)
-{
-    uint8_t oid = args[0];
-    struct spidev_s *spi = spidev_oid_lookup(oid);
-    uint8_t data_len = args[1];
-    uint8_t *data = (void*)(size_t)args[2];
-    spidev_transfer(spi, 1, data_len, data);
-    sendf("spi_transfer_response oid=%c response=%*s", oid, data_len, data);
-}
-DECL_COMMAND(command_spi_transfer, "spi_transfer oid=%c data=%*s");
-
-void
-command_spi_send(uint32_t *args)
-{
-    struct spidev_s *spi = spidev_oid_lookup(args[0]);
-    uint8_t data_len = args[1];
-    uint8_t *data = (void*)(size_t)args[2];
-    spidev_transfer(spi, 0, data_len, data);
-}
-DECL_COMMAND(command_spi_send, "spi_send oid=%c data=%*s");
-
-
-/****************************************************************
- * Shutdown handling
- ****************************************************************/
-
-struct spidev_shutdown_s {
-    struct spidev_s *spi;
-    uint8_t shutdown_msg_len;
-    uint8_t shutdown_msg[];
-};
-
-void
-command_config_spi_shutdown(uint32_t *args)
-{
-    struct spidev_s *spi = spidev_oid_lookup(args[1]);
-    uint8_t shutdown_msg_len = args[2];
-    struct spidev_shutdown_s *sd = oid_alloc(
-        args[0], command_config_spi_shutdown, sizeof(*sd) + shutdown_msg_len);
-    sd->spi = spi;
-    sd->shutdown_msg_len = shutdown_msg_len;
-    uint8_t *shutdown_msg = (void*)(size_t)args[3];
-    memcpy(sd->shutdown_msg, shutdown_msg, shutdown_msg_len);
-}
-DECL_COMMAND(command_config_spi_shutdown,
-             "config_spi_shutdown oid=%c spi_oid=%c shutdown_msg=%*s");
-
-void
-spidev_shutdown(void)
-{
-    // Cancel any transmissions that may be in progress
     uint8_t oid;
-    struct spidev_s *spi;
-    foreach_oid(oid, spi, command_config_spi) {
-        if (spi->flags & SF_HAVE_PIN)
-            gpio_out_write(spi->pin, 1);
-    }
+    struct fpga_s *f;
+    int wake = 0;
+    int i;
 
-    // Send shutdown messages
-    struct spidev_shutdown_s *sd;
-    foreach_oid(oid, sd, command_config_spi_shutdown) {
-        spidev_transfer(sd->spi, 0, sd->shutdown_msg_len, sd->shutdown_msg);
+    foreach_oid(oid, f, command_config_fpga) {
+        if (f->state == FS_INIT_FLASH) {
+            uint32_t data = 0x03000000;
+
+            // setup flash for reading
+            gpio_out_write(f->cs, 0);
+            for (i = 0; i < 32; ++i) {
+                gpio_out_write(f->mosi, data & 0x80000000);
+                data <<= 1;
+                ndelay(200);
+                gpio_out_write(f->clk, 1);
+                ndelay(200);
+                gpio_out_write(f->clk, 0);
+            }
+
+            f->state = FS_INIT_FPGA;
+        } else if (f->state == FS_INIT_FPGA) {
+            // initialize fpga
+            gpio_out_write(f->program, 0);
+            ndelay(70);
+            // check for initn to go low
+            if (gpio_in_read(f->init))
+                shutdown("fpga config error (init not low)");
+            ndelay(40);
+            gpio_out_write(f->program, 1);
+            ndelay(20);
+            // check for initn to go high and done low
+            if (!gpio_in_read(f->init))
+                shutdown("fpga config error (init not high)");
+            if (gpio_in_read(f->done))
+                shutdown("fpga config error (done not low)");
+
+            f->cnt = 0;
+            f->state = FS_TRANSFER;
+        } else if (f->state == FS_TRANSFER) {
+            // transfer 1024 bits from flash to fpga
+            for (i = 0; i < 1024; ++i) {
+                gpio_out_write(f->di, gpio_in_read(f->miso));
+                ndelay(200);
+                gpio_out_write(f->clk, 1);
+                ndelay(200);
+                gpio_out_write(f->clk, 0);
+            }
+
+            // check for error
+            if (!gpio_in_read(f->init))
+                shutdown("fpga config error (init asserted)");
+            // check for done
+            if (gpio_in_read(f->done))
+                f->state = FS_SEND_RESPONSE;
+            // check if max config length is exceeded
+            if (++f->cnt >= 16384)
+                shutdown("fpga config error (done not set)");
+        } else if (f->state == FS_SEND_RESPONSE) {
+            sendf("fpga_init_done oid=%c", oid);
+            f->state = FS_INITIALIZED;
+        }
+        if (f->state != FS_INITIALIZED)
+            wake = 1;
     }
+    if (wake)
+        sched_wake_task(&fpga_wake);
 }
+DECL_TASK(fpga_task);
+
+#if 0
 DECL_SHUTDOWN(spidev_shutdown);
 #endif
