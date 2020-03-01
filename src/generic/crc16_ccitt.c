5d2c69b712bc058b9c9d830ef2bc017fcfd7080a
Arne Jansen <arne@die-jansens.de>
2020-03-01 10:59:56 +0000

    add crc16 for one byte

--- src/generic/crc16_ccitt.c
+++ src/generic/crc16_ccitt.c
@@ -6,17 +6,26 @@
 
 #include "misc.h" // crc16_ccitt
 
+
+uint16_t
+crc16_ccitt_one(uint8_t data, uint16_t crc)
+{
+    data ^= crc & 0xff;
+    data ^= data << 4;
+    crc = ((((uint16_t)data << 8) | (crc >> 8)) ^ (uint8_t)(data >> 4)
+           ^ ((uint16_t)data << 3));
+
+    return crc;
+}
+
 // Implement the standard crc "ccitt" algorithm on the given buffer
 uint16_t
 crc16_ccitt(uint8_t *buf, uint_fast8_t len)
 {
     uint16_t crc = 0xffff;
-    while (len--) {
-        uint8_t data = *buf++;
-        data ^= crc & 0xff;
-        data ^= data << 4;
-        crc = ((((uint16_t)data << 8) | (crc >> 8)) ^ (uint8_t)(data >> 4)
-               ^ ((uint16_t)data << 3));
-    }
+
+    while (len--)
+        crc = crc16_ccitt_one(*buf++, crc);
+
     return crc;
 }
