45fbe9988da5ec3c0a6fc25fb68766882a5cfd8f
Arne Jansen <arne@die-jansens.de>
2020-03-11 07:57:31 +0000

    mcu.py: add fid counter
    Preparatory commit for fpga support. Manage the fpga ids in mcu.py.
    
    Signed-off-by: Arne Jansen <arne@die-jansens.de>

--- klippy/mcu.py
+++ klippy/mcu.py
@@ -442,12 +442,16 @@ class MCU:
         self._mcu_tick_avg = 0.
         self._mcu_tick_stddev = 0.
         self._mcu_tick_awake = 0.
+        self._fid = 0
         # Register handlers
         printer.register_event_handler("klippy:connect", self._connect)
         printer.register_event_handler("klippy:mcu_identify",
                                        self._mcu_identify)
         printer.register_event_handler("klippy:shutdown", self._shutdown)
         printer.register_event_handler("klippy:disconnect", self._disconnect)
+    def create_fid(self):
+        self._fid += 1
+        return self._fid - 1
     # Serial callbacks
     def _handle_mcu_stats(self, params):
         count = params['count']
