b6f51c5841e32766f5b87386b787fdeadc3e93d7
Arne Jansen <arne@die-jansens.de>
2020-04-12 13:58:17 +0000

    fix move_count in klippy

--- klippy/mcu.py
+++ klippy/mcu.py
@@ -592,7 +592,7 @@ class MCU:
             # Already configured - send init commands
             self._send_config(config_params['crc'])
         # Setup steppersync with the move_count returned by get_config
-        move_count = config_params['move_count']
+        self.move_count = move_count = config_params['move_count']
         ffi_main, ffi_lib = chelper.get_ffi()
         self._steppersync = ffi_main.gc(
             ffi_lib.steppersync_alloc(self._serial.serialqueue,
